4c27085152128995ef23937490151331c49ae27a
moonrock <tinysrc@163.com>
2013-10-30 16:22:19 +0800

    add logic_thread

---
+++ common_src/logic_thread.h
@@ -0,0 +1,22 @@
+#ifndef LOGIC_THREAD_H_INCLUDED
+#define LOGIC_THREAD_H_INCLUDED
+
+#include "net.h"
+
+#include <pthread.h>
+
+typedef struct {
+    pthread_t thread;
+    user_callback cb;
+    struct event_base *base;
+    struct event notify_event;
+    int notify_receive_fd;
+    int notify_send_fd;
+} LOGIC_THREAD;
+
+void logic_thread_init(LOGIC_THREAD *logic, user_callback *cb);
+int logic_thread_add_rpc_event(LOGIC_THREAD *logic, conn *c, unsigned char *msg, size_t sz);
+int logic_thread_add_connect_event(LOGIC_THREAD *logic, conn *c);
+int logic_thread_add_disconnect_event(LOGIC_THREAD *logic, conn *c);
+
+#endif /* LOGIC_THREAD_H_INCLUDED */
